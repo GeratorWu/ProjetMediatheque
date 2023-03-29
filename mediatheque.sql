1740341de0737584462eb8177ad8bfd05c5e324a
Patrick Wu <118174626+GeratorWu@users.noreply.github.com>
2023-03-29 12:13:00 +0200

    Update mediatheque.sql

--- mediatheque.sql
+++ mediatheque.sql
@@ -5,7 +5,7 @@ CREATE DATABASE mediatheque;
 USE mediatheque;
 
 -- Détruire les tables;
-DROP TABLE Abonne, DVD, Livre;
+DROP TABLE Abonne, DVD;
 
 -- Création de la table Abonne
 CREATE TABLE Abonne (
@@ -20,16 +20,6 @@ CREATE TABLE DVD (
     idDvd INT PRIMARY KEY,
     titre VARCHAR(255) NOT NULL,
     adulte BOOLEAN,
-    emprunteur INT,
-    reserveur INT,
-    FOREIGN KEY (emprunteur) REFERENCES abonne(idAbonne),
-    FOREIGN KEY (reserveur) REFERENCES abonne(idAbonne)
-);
-
--- Création de la table Live
-CREATE TABLE Livre (
-    idLiv INT PRIMARY KEY,
-    titre VARCHAR(255) NOT NULL,
     emprunteur INT,
     reserveur INT,
     FOREIGN KEY (emprunteur) REFERENCES abonne(idAbonne),
@@ -57,17 +47,6 @@ INSERT INTO DVD (idDvd, titre, adulte, emprunteur, reserveur) VALUES
     (9, 'Le Silence des Agneaux', 1, NULL, NULL),
     (10, 'Les Évadés', 0, NULL, NULL);
 
-INSERT INTO Livre (idLiv, titre, emprunteur, reserveur) VALUES
-    (1, 'Le Petit Prince', 3, NULL),
-    (2, 'Harry Potter à l école des sorciers', NULL, NULL),
-    (3, 'Les Misérables', 2, NULL),
-    (4, '1984', NULL, 3),
-    (5, 'Le Hobbit', NULL, 4),
-    (6, 'L Alchimiste', NULL, NULL),
-    (7, 'Le Comte de Monte-Cristo', 2, NULL),
-    (8, 'La Nuit des temps', NULL, 1),
-    (9, 'Le Cycle de Dune', 2, NULL),
-    (10, 'Le Cycle Fondation', NULL, NULL);
 -- ajout contrainte
 ALTER TABLE DVD ADD CONSTRAINT emprunt_reserve CHECK (
     (emprunteur IS NULL OR reserveur IS NULL) OR
@@ -75,10 +54,5 @@ ALTER TABLE DVD ADD CONSTRAINT emprunt_reserve CHECK (
     (emprunteur IS NULL AND reserveur IS NOT NULL)
 );
 
-ALTER TABLE Livre ADD CONSTRAINT emprunt_reserve CHECK (
-    (emprunteur IS NULL OR reserveur IS NULL) OR
-    (emprunteur IS NOT NULL AND reserveur IS NULL) OR
-    (emprunteur IS NULL AND reserveur IS NOT NULL)
-);
 -- COMMIT
 COMMIT;
